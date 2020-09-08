850a680ab7bc916bfd28e082b376f024d2dd5083
ytodo <je3hcz@gmail.com>
2020-09-08 09:43:00 +0900

    D*SWITCH v0.0.1 - Debug

--- GetActive.c
+++ GetActive.c
@@ -34,6 +34,9 @@ void	getactive(void)
 			break;
 		}
 
+                /* ファイルを読む前に更新する */
+                system("sudo /var/www/cgi-bin/repActive");
+
 		/* コマンドの標準出力オープン */
 		if ((fp = fopen(active_tbl, "r")) == NULL)
 		{
